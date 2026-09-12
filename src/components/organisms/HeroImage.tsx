0d76e3780d70fcd61323cb8687fe3625c3f1cad9
Pablo Zallio <pablozalliodev@gmail.com>
2026-09-12 18:32:30 +0200

    correcting paddings II

--- src/components/organisms/HeroImage.tsx
+++ src/components/organisms/HeroImage.tsx
@@ -2,11 +2,11 @@
 const HeroImage = () => {
   return (
     <div className=' w-full h-64 mt-16 ' style={{ backgroundImage: "url('/images/hero_image.webp')", backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center'  }}>
-      <div className=" relative w-full h-full bg-font/60 ">
+      <div className=" relative w-full h-full bg-font/20 ">
       <div className=" absolute bottom-6 left-5    ">
             <div className=" text-3xl font-black leading-6 ">
             <p className=" text-cream ">Come bien</p>
-            <p className=" text-brand-light ">Vive mas</p>
+            <p className=" text-brand ">Vive mas</p>
             </div>
             <p className=" font-extralight text-cream ">Carta de temporada</p>
       </div>
